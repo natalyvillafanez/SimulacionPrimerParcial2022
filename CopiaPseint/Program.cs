b6b47f03dc61e54f94edbfbe2f64f951e65d92e7
Usuario <Usuario@LAPTOP-O5Q2PI44>
2022-07-04 19:57:09 -0300

    Agregar archivos de proyecto.

---
+++ CopiaPseint/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CopiaPseint
+{
+	class Program
+	{
+
+		static bool Verificacion(int inicio, int stop, int num)
+		{
+			if ((num >= inicio) && (num <= stop))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			int hora, minutos, segundos;
+			string pmam, siNo;
+
+			Console.Write(("Ingrese la HORA: "));
+			hora = int.Parse(Console.ReadLine());
+			Console.Write(("Ingrese los MINUTOS: "));
+			minutos = int.Parse(Console.ReadLine());
+			Console.Write(("Ingrese los SEGUNDOS: "));
+			segundos = int.Parse(Console.ReadLine());
+
+			if ((Verificacion(0, 12, hora) == true) && (Verificacion(0, 59, minutos) == true) && (Verificacion(0, 59, segundos) == true))
+			{
+				Console.Write(("¿PM o AM?"));
+				pmam = Console.ReadLine();
+
+				if ((pmam.ToUpper()=="AM") || (pmam.ToUpper() == "PM"))
+				{
+					Console.WriteLine("La hora es {0}:{1}:{2} {3}", hora, minutos, segundos, pmam.ToUpper());
+					Console.WriteLine("¿Desea ver la hora ingresada en formato de 24 hs? SI/NO");
+					siNo = Console.ReadLine();
+
+					if ((siNo.ToUpper() == "SI"))
+					{
+						if ((pmam.ToUpper() == "PM"))
+						{
+							Console.WriteLine("La hora en formato 24 hs es {0}:{1}:{2}",(hora + 12), minutos, segundos);
+						}
+						else
+						{
+							Console.WriteLine("La hora en formato 24 hs es {0}:{1}:{2}", hora, minutos, segundos);
+						}
+					}
+					else
+
+					{
+						if ((siNo.ToUpper() == "NO"))
+						{
+							Console.WriteLine("Fin del programa...");
+						}
+						else
+						{
+							Console.WriteLine("Ingreso incorrecto, por favor verifique...");
+						}
+					}
+				}
+				else
+				{
+					Console.WriteLine("Ingreso incorrecto, por favor verifique...");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Hora ingresada fuera de rango, por favor verifique...");
+			}
+		}
+
+	}
+
+}
+
---
+++ SimulacionPrimerParcial2022/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SimulacionPrimerParcial2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool Validacion(int x, int y, int numero)
+            {
+                if ((numero>=x) && (numero<=y))
+                {
+                    var validacion = true;
+                    return validacion;
+                }
+                else
+                {
+                    var validacion = false;
+                    return validacion;
+                }
+            }
+            Console.Write("Ingrese la HORA: ");
+            var hora = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese los MINUTOS: ");
+            var minutos = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese los SEGUNDOS: ");
+            var segundos = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese AM o PM: ");
+            var ampm = Console.ReadLine();
+
+
+
+            if ((Validacion(0,12,hora) == true) && (Validacion(0,60,minutos) == true) && (Validacion(0,60,segundos) == true))
+            {
+                if (ampm.ToUpper() == "AM")
+                {
+                    Console.Write("La hora ingresada es {0}:{1}:{2} {3}", hora, minutos, segundos, ampm.ToUpper());
+                }
+                else
+                {
+                    if (ampm.ToUpper() == "PM")
+                    {
+                        hora = hora + 12;
+                        Console.Write("La hora ingresada es {0}:{1}:{2} {3}", hora, minutos, segundos, ampm.ToUpper());
+                    }
+                    else
+                    {
+                        Console.Write("Error al ingresar los datos...");
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("La hora que se ha ingresado es incorrecta, por favor verifique...");
+            }
+            
+
+        }
+    }
+}
